764397a60c0b2b1812f74c3a1c79b31a63977f9e
tristan <mousy93@hotmail.com>
2021-08-12 20:52:58 +0800

    landing page styleing done enough

---
+++ react-express-yourself/src/components/LandingPageBody/LandingPageBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./LandingPageBody.css"
+
+export default function LandingPageBody() {
+    return (
+        <div id="landingBackground">
+            <div id="circleBackground"></div>
+            <div id="LandingDescription"></div>
+        </div>
+    )
+}
