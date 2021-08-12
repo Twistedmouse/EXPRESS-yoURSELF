7001c4de6109c8f5fc096f6b7b3fff85b82f549e
tristan <mousy93@hotmail.com>
2021-08-12 22:46:37 +0800

    more landing page css; tested login routing read docd docssssdocs!

--- react-express-yourself/src/components/LandingPageBody/LandingPageBody.jsx
+++ react-express-yourself/src/components/LandingPageBody/LandingPageBody.jsx
@@ -5,7 +5,9 @@ export default function LandingPageBody() {
     return (
         <div id="landingBackground">
             <div id="circleBackground"></div>
-            <div id="LandingDescription"></div>
+            <div id="LandingDescription">
+                <p> Lorem ipsum dolor sit amet, consectetur adip</p>
+            </div>
         </div>
     )
 }
--- react-express-yourself/src/components/LoginForm/LoginForm.jsx
+++ react-express-yourself/src/components/LoginForm/LoginForm.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
 import "./LoginForm.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
 
-export default function LoginForm() {
+import Auth from '../../utils/auth';
+
+const LoginForm = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+  
+    // update state based on form input changes
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+  
+    // submit form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      console.log(formState);
+      try {
+        const { data } = await login({
+          variables: { ...formState },
+        });
+  
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+  
+      // clear form values
+      setFormState({
+        email: '',
+        password: '',
+      });
+    };
+
     return (
         <div id="background">
             <article className="login-form">
                 <h1 id="loginTitle">EXPRESS yoURSELF</h1>
                 <h2 id="signinH2">Sign in:</h2>
                 <div className="form-box">
-                <form >
+                <form onSubmit={handleFormSubmit}>
                 <div>
                     <label>Username: </label>
-                    <input id="username" type="text" placeholder="Enter Username"></input>
+                    <input id="username" name="email" type="text" placeholder="Enter Username"  value={formState.email}
+                  onChange={handleChange}></input>
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input id="password" type="password" placeholder="Enter Password"></input>
+                    <input id="password" name="password" type="password" placeholder="Enter Password" value={formState.password}
+                  onChange={handleChange}></input>
                 </div>
             </form>
             <div className="buttons">
@@ -26,5 +67,8 @@ export default function LoginForm() {
             </article>
         </div>
     )
+    
 }
+
+export default LoginForm
 
