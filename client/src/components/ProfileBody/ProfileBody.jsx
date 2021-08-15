0f2286277fa3cc1e9d6466ff0cd1b08863291a82
tristan <mousy93@hotmail.com>
2021-08-16 02:55:30 +0800

    merge

--- client/src/components/ProfileBody/ProfileBody.jsx
+++ client/src/components/ProfileBody/ProfileBody.jsx
@@ -1,45 +1,52 @@
-import React from 'react';
-import {useQuery} from '@apollo/client'
-import './ProfileBody.css'
-import { QUERY_ME } from '../../utils/queries'
-import  Auth  from '../../utils/auth'
+import React from "react";
+import { useQuery } from "@apollo/client";
+import "./ProfileBody.css";
+import { QUERY_ME } from "../../utils/queries";
+import Auth from "../../utils/auth";
 // import { Redirect, useParams } from 'react-router-dom';
 
-
-
-
 export default function ProfileBody() {
-    
-    
-    
-    // const { loading, data } = useQuery(QUERY_ME);
-    
+  const { loading, data } = useQuery(QUERY_ME);
+  const user = data?.me || {};
 
-    if (Auth.loggedIn()) {
-      
-      
-      // console.log(Auth.getUser())
-        return (
-          <div>
-              <div id="landingBackground">
-              <div id="circleBackground"></div>
-              <div id="LandingDescription">
-                  <div id="profileInfo">
-                      <p>Username: {Auth.getUser().data.username}</p>
-                      <button className="editProfile" type="button" style={{ cursor: 'pointer' }}>EDIT</button>
-                      <p>Description: </p>
-                      <button className="editProfile" type="button" style={{ cursor: 'pointer' }}>EDIT</button>
-                  </div>
-                 
-                  <button className="deleteProfile" type="button" style={{ cursor: 'pointer' }}>DELETE PROFILE</button>
-              </div>
-              
+  console.log(user);
+  if (Auth.loggedIn()) {
+    // console.log(Auth.getUser())
+    return (
+      <div>
+        <div id="landingBackground">
+          <div id="LandingDescription">
+            <div id="profileInfo">
+              <p>Username: {Auth.getUser().data.username}</p>
+              <button
+                className="editProfile"
+                type="button"
+                style={{ cursor: "pointer" }}
+              >
+                EDIT
+              </button>
+              <p>Description: </p>
+              <button
+                className="editProfile"
+                type="button"
+                style={{ cursor: "pointer" }}
+              >
+                EDIT
+              </button>
+            </div>
+
+            <button
+              className="deleteProfile"
+              type="button"
+              style={{ cursor: "pointer" }}
+            >
+              DELETE PROFILE
+            </button>
           </div>
-          </div>
-      )
-      }  
-      
-        
-    }
-
-
+        </div>
+      </div>
+    );
+  } else {
+    <p>You must be logged in to view! </p>;
+  }
+}
