60a04d8187405bcc23e8c8dbc826c9cee1c94109
tristan <mousy93@hotmail.com>
2021-09-07 20:11:29 +0800

    bubble chart from html test re written for react and working now work on styling it ect

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,60 +1,106 @@
 import React, {Component} from "react";
 import "../darkGrayTheme.css";
-import {Bubble, Pie} from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import {Bubble} from 'react-chartjs-2';
 
-class Chart extends Component{
-    // we get our data stored as a state in our constructor
-    constructor(props){
-      super(props);
-      this.state = {
-        chartData:props.chartData
-      }
-    }
-  
-    static defaultProps = {
-      displayTitle:true,
-      displayLegend: true,
-      legendPosition:'right',
-      location:'City'
-    }
-  
-    render(){
-      return (
-        <div className="chart">
-          <Bubble
-            data={this.state.chartData}
-            options={{
-              title:{
-                display:this.props.displayTitle,
-                text:'Largest Cities In '+this.props.location,
-                fontSize:25
+
+
+        //setup block
+        const data = {
+          datasets: [{
+              label: 'Happy' ,
+              data: [
+                  {
+                      x: 1.5, y:90, r:20 
+                  },
+                  {
+                      x: 10, y:62, r: 12
+                  }
+              ],
+              backgroundColor: [
+                  '	rgb(255, 255, 229,0.8)',
+              ],
+              borderColor: [
+                  'orange',
+              ],
+              borderWidth: 1,
+              clip: {left: false, top: false, right: false, bottom: false}
+          },
+          {
+              label: 'Sad' ,
+              data: [
+                  {
+                      x: 3, y:50, r:10, 
+                  },
+                  {
+                      x: 10, y:84, r: 15,
+                  }
+              ],
+              backgroundColor: [
+                      'rgb(230, 255, 255,0.8)'
+              ],
+              borderColor: [
+                  'blue'
+              ],
+              borderWidth: 1,
+              clip: {left: false, top: false, right: false, bottom: false}
+          }
+          
+      ]}; 
+
+    
+
+        // config block
+        const config = {
+          type: 'bubble',
+          data,
+          options: {
+              layout:{
+                  
+                  padding: { right: 20}
               },
-              legend:{
-                display:this.props.displayLegend,
-                position:this.props.legendPosition
-              }
-            }}
-          />
+              scales: {
+                  x:{
+                     title: {
+                         display: true, 
+                         text: 'time in seconds'
+                      },
+                      min: 1, 
+                      max: 30,
 
-  
-          {/* <Pie
-            data={this.state.chartData}
-            options={{
-              title:{
-                display:this.props.displayTitle,
-                text:'Largest Cities In '+this.props.location,
-                fontSize:25
-              },
-              legend:{
-                display:this.props.displayLegend,
-                position:this.props.legendPosition
-              }
-            }}
-          /> */}
-        </div>
-      )
-    }
-  }
-  
-  export default Chart;
+                           // look into type time in future leave like this for now for demo so dont have to change data types for now
+                     
+                      // type: 'time',
+                      // time: {      
+                      //     unit: 'second'
+                      // }
 
+                  },
+                  y: {
+                  
+                      title: {
+                         display: true, 
+                         text: '% of emotion'
+                      },
+                      min: 10, 
+                      max: 100,
+                     
+                  }
+              }
+          }
+      };
+
+
+        //  render block
+        const renderChart = () => (
+          <>
+    
+    <div class="chartContainer">
+        <canvas id="myChart"></canvas>
+        <Bubble data={data} options={config} />
+    </div>
+  </>
+        )
+
+
+export default renderChart;
