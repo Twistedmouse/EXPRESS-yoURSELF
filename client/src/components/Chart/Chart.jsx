62ac8025370df3b82c70e163675e4a246e622bd8
tristan <mousy93@hotmail.com>
2021-09-07 21:50:28 +0800

    proile page updated for coming soon; options for chart working now

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,106 +1,95 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../darkGrayTheme.css";
-import Chart from 'chart.js/auto';
-import {Bubble} from 'react-chartjs-2';
+import { Bubble } from "react-chartjs-2";
 
+//setup block
+const data = {
+  datasets: [
+    {
+      label: "Happy",
+      data: [
+        {
+          x: 1.5,
+          y: 90,
+          r: 20,
+        },
+        {
+          x: 10,
+          y: 62,
+          r: 12,
+        },
+      ],
+      backgroundColor: ["	rgb(255, 255, 229,0.8)"],
+      borderColor: ["orange"],
+      borderWidth: 1,
+      clip: { left: false, top: false, right: false, bottom: false },
+    },
+    {
+      label: "Sad",
+      data: [
+        {
+          x: 3,
+          y: 50,
+          r: 10,
+        },
+        {
+          x: 10,
+          y: 84,
+          r: 15,
+        },
+      ],
+      backgroundColor: ["rgb(230, 255, 255,0.8)"],
+      borderColor: ["blue"],
+      borderWidth: 1,
+      clip: { left: false, top: false, right: false, bottom: false },
+    },
+  ],
+};
 
+const options = {
+  legend: {
+    labels: {
+      fontColor: "pink",
+    },
+  },
+  layout: {
+    padding: { right: 20 },
+  },
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: "time in seconds",
+      },
+      min: 1,
+      max: 30,
 
-        //setup block
-        const data = {
-          datasets: [{
-              label: 'Happy' ,
-              data: [
-                  {
-                      x: 1.5, y:90, r:20 
-                  },
-                  {
-                      x: 10, y:62, r: 12
-                  }
-              ],
-              backgroundColor: [
-                  '	rgb(255, 255, 229,0.8)',
-              ],
-              borderColor: [
-                  'orange',
-              ],
-              borderWidth: 1,
-              clip: {left: false, top: false, right: false, bottom: false}
-          },
-          {
-              label: 'Sad' ,
-              data: [
-                  {
-                      x: 3, y:50, r:10, 
-                  },
-                  {
-                      x: 10, y:84, r: 15,
-                  }
-              ],
-              backgroundColor: [
-                      'rgb(230, 255, 255,0.8)'
-              ],
-              borderColor: [
-                  'blue'
-              ],
-              borderWidth: 1,
-              clip: {left: false, top: false, right: false, bottom: false}
-          }
-          
-      ]}; 
+      // look into type time in future leave like this for now for demo so dont have to change data types for now you need an adapter for the api to read timestamps too
+      // type: 'time',
+      // time: {
+      //     unit: 'second'
+      // }
+    },
+    y: {
+      title: {
+        display: true,
+        text: "% of emotion",
+      },
+      min: 10,
+      max: 100,
+    },
+  },
+};
+// };
 
-    
-
-        // config block
-        const config = {
-          type: 'bubble',
-          data,
-          options: {
-              layout:{
-                  
-                  padding: { right: 20}
-              },
-              scales: {
-                  x:{
-                     title: {
-                         display: true, 
-                         text: 'time in seconds'
-                      },
-                      min: 1, 
-                      max: 30,
-
-                           // look into type time in future leave like this for now for demo so dont have to change data types for now
-                     
-                      // type: 'time',
-                      // time: {      
-                      //     unit: 'second'
-                      // }
-
-                  },
-                  y: {
-                  
-                      title: {
-                         display: true, 
-                         text: '% of emotion'
-                      },
-                      min: 10, 
-                      max: 100,
-                     
-                  }
-              }
-          }
-      };
-
-
-        //  render block
-        const renderChart = () => (
-          <>
-    
-    <div class="chartContainer">
-        <canvas id="myChart"></canvas>
-        <Bubble data={data} options={config} />
+//  render block
+const renderChart = () => (
+  <>
+    <div className="chartContainer">
+      <canvas id="myChart"></canvas>
+      <Bubble data={data} options={options} />
     </div>
   </>
-        )
-
+);
 
 export default renderChart;
--- client/src/components/ProfileBody/ProfileBody.jsx
+++ client/src/components/ProfileBody/ProfileBody.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import "./ProfileBody.css";
+import "../darkGrayTheme.css";
 import { QUERY_ME } from "../../utils/queries";
 import Auth from "../../utils/auth";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 export default function ProfileBody() {
-  const { username } = useParams()
-  const { loading, data } = useQuery(QUERY_ME, { 
-    variables: {username: username}
+  const { username } = useParams();
+  const { loading, data } = useQuery(QUERY_ME, {
+    variables: { username: username },
   });
   const user = data?.me || {};
 
-  if(loading){
-    return <p id='login'>Loading...</p>
+  if (loading) {
+    return <p id="login">Loading...</p>;
   }
 
-  if(!Auth.loggedIn()){
-    return <div id="circleBackground">
-      <p id='login'><br></br><br></br>YOU MUST BE LOGGED IN TO VIEW PROFILE.</p>
-    </div>
-    
+  if (!Auth.loggedIn()) {
+    return (
+      <div id="circleBackground">
+        <p id="login">
+          <br></br>
+          <br></br>YOU MUST BE LOGGED IN TO VIEW PROFILE.
+        </p>
+      </div>
+    );
   }
 
   // console.log(user.username);
@@ -29,6 +33,10 @@ export default function ProfileBody() {
     return (
       <div>
         <div id="landingBackground">
+          <p id="comingSoon">
+            SAVED CHARTS COMING SOON COME BACK LATER FOR PROFILE SECTION NOT
+            INCLUDED IN DEMO APP!
+          </p>
           <div id="InfoDescription">
             <div id="profileInfo">
               <p>Username: {Auth.getUser().data.username}</p>
@@ -53,7 +61,7 @@ export default function ProfileBody() {
             <button
               className="deleteProfile"
               type="button"
-              style={{ cursor: "pointer" }} 
+              style={{ cursor: "pointer" }}
             >
               DELETE PROFILE
             </button>
